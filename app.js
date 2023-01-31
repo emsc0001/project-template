53ddacc98bd4a9e341e8261c3d95e021ee07b386
emsc0001 <124031667+emsc0001@users.noreply.github.com>
2023-01-31 12:57:14 +0100

    SayHello 1

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("app.js is running 🎉");
+function sayHello(Emil) {
+  document.querySelector("h3").textContent =
+    "SayHello" + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
